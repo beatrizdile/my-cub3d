b9dbd0937631189efe070ee8616e4fb61e7faa74
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-03-04 19:11:01 -0300

    base code on main

--- src/mandatory/cub3d.c
+++ src/mandatory/cub3d.c
@@ -12,16 +12,6 @@
 
 #include "cub3d.h"
 
-void	ft_hook(void *cub3d)
-{
-	for(int i = 0; i < 30; i++)
-		for(int j = 0; j < 30; j++)
-			mlx_put_pixel(((t_cub3d *)cub3d)->image, i, j, 0xFF0000FF);
-	for(int i = 0; i < 15; i++)
-		for(int j = 0; j < 15; j++)
-			mlx_put_pixel(((t_cub3d *)cub3d)->image2, i, j, 0x0000FFFF);
-}
-
 // void	initialize(t_cub3d *cub3d)
 // {
 // 	cub3d->mlx_ptr = mlx_init(WIDTH, HEIGHT, "cub3D", true);
@@ -45,52 +35,54 @@ void	ft_hook(void *cub3d)
 // 	}
 // }
 
-int	main()
+# include <stdio.h>
+# include "libft.h"
+# include <MLX42/MLX42.h>
+
+# define WIDTH 1200
+# define HEIGHT 800
+# define MINI_MAP_TILE_SIZE 30
+# define RESIZE_WINDOW false
+
+typedef struct s_cub3d
 {
 	mlx_t			*mlx_ptr;
 	mlx_image_t		*image;
-	mlx_image_t		*image2;
+}				t_cub3d;
 
-	mlx_ptr = mlx_init(WIDTH, HEIGHT, "cub3D", RESIZE_WINDOW);
-	if (mlx_ptr == NULL)
-	{
-		puts(mlx_strerror(mlx_errno));
-		exit(EXIT_FAILURE);
-	}
-	image = mlx_new_image(mlx_ptr, WIDTH, HEIGHT);
-	if (image == NULL)
-	{
-		mlx_close_window(mlx_ptr);
-		puts(mlx_strerror(mlx_errno));
-		exit(EXIT_FAILURE);
-	}
-	image2 = mlx_new_image(mlx_ptr, WIDTH, HEIGHT);
-	if (image == NULL)
-	{
-		mlx_close_window(mlx_ptr);
-		puts(mlx_strerror(mlx_errno));
-		exit(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx_ptr, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx_ptr);
-		puts(mlx_strerror(mlx_errno));
-		exit(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx_ptr, image2, 0, 0) == -1)
-	{
-		mlx_close_window(mlx_ptr);
-		puts(mlx_strerror(mlx_errno));
-		exit(EXIT_FAILURE);
-	}
+void	ft_hook(void *cub3d)
+{
+	for(int i = 0; i < 30; i++)
+		for(int j = 0; j < 30; j++)
+			mlx_put_pixel(((t_cub3d *)cub3d)->image, i, j, 0xFF0000FF);
+}
 
+int	main()
+{
 	t_cub3d		cub3d;
-	cub3d.mlx_ptr = mlx_ptr;
-	cub3d.image = image;
-	cub3d.image2 = image2;
 
-	mlx_loop_hook(mlx_ptr, ft_hook, &cub3d);
-	mlx_loop(mlx_ptr);
-	mlx_terminate(mlx_ptr);
+	cub3d.mlx_ptr = mlx_init(WIDTH, HEIGHT, "cub3D", RESIZE_WINDOW);
+	if (cub3d.mlx_ptr == NULL)
+	{
+		puts(mlx_strerror(mlx_errno));
+		exit(EXIT_FAILURE);
+	}
+	cub3d.image = mlx_new_image(cub3d.mlx_ptr, WIDTH, HEIGHT);
+	if (cub3d.image == NULL)
+	{
+		mlx_close_window(cub3d.mlx_ptr);
+		puts(mlx_strerror(mlx_errno));
+		exit(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(cub3d.mlx_ptr, cub3d.image, 0, 0) == -1)
+	{
+		mlx_close_window(cub3d.mlx_ptr);
+		puts(mlx_strerror(mlx_errno));
+		exit(EXIT_FAILURE);
+	}
+
+	mlx_loop_hook(cub3d.mlx_ptr, ft_hook, &cub3d);
+	mlx_loop(cub3d.mlx_ptr);
+	mlx_terminate(cub3d.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
