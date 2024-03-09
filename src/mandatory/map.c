ca515d4b4995e8f75d8eeedaef0aae56c6aad7e0
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-03-09 19:06:19 -0300

    feat: rendering walls and floors on mini-map

--- src/mandatory/map.c
+++ src/mandatory/map.c
@@ -6,11 +6,12 @@
 /*   By: bedos-sa <bedos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:48:40 by bedos-sa          #+#    #+#             */
-/*   Updated: 2024/03/07 19:06:23 by bedos-sa         ###   ########.fr       */
+/*   Updated: 2024/03/09 19:05:17 by bedos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
 
 static char	*map[5] = {	
 						"1111111",
@@ -20,6 +21,7 @@ static char	*map[5] = {
 						"1111111"
 					};
 
+
 void	read_map(t_cub3d *cub3d)
 {
 	cub3d->map = map;
@@ -27,36 +29,35 @@ void	read_map(t_cub3d *cub3d)
 	cub3d->map_x = 7;
 }
 
+static uint32_t	get_mini_map_color(t_cub3d *cub3d, t_vector *point)
+{
+	uint32_t color;
+	int y = point->y;
+	int x = point->x;
+
+	color = 0;
+	if (cub3d->map[y][x] == '1')
+		color = WALL_TILE_COLOR;
+	else if (cub3d->map[y][x] == '0' || cub3d->map[y][x] == 'N')
+		color = FLOOR_TILE_COLOR;
+	return (color);
+}
+
 void	render_mini_map(t_cub3d *cub3d)
 {
 	t_vector	point;
 	t_vector	map_point;
 
-	point.y = 0;
-	for(size_t y = 0; y < cub3d->map_y; point.y++)
+	for(point.y = 0; point.y < cub3d->map_y; point.y++)
 	{
-		point.x = 0;
-		for(size_t x = 0; x < cub3d->map_x; point.x++)
+		printf("%s\n", cub3d->map[(int)point.y]);
+		for(point.x = 0; point.x < cub3d->map_x; point.x++)
 		{
+			uint32_t color = get_mini_map_color(cub3d, &point);
 			map_point.y = point.y * MINI_MAP_TILE_SIZE;
 			map_point.x = point.x * MINI_MAP_TILE_SIZE;
-			// printf("%c", cub3d->map[y][x]);
-			// if (cub3d->map[y][x] == WALL_CHAR)
-			// 	mlx_put_pixel(((t_cub3d *)cub3d)->image, (i*MINI_MAP_TILE_SIZE), \
-			// 	(j*MINI_MAP_TILE_SIZE), WALL_TILE_COLOR);
-			// else if (cub3d->map[y][x] == FLOOR_CHAR)
-			// 	mlx_put_pixel(((t_cub3d *)cub3d)->image, (i*MINI_MAP_TILE_SIZE), \
-			// 	(j*MINI_MAP_TILE_SIZE), FLOOR_TILE_COLOR);
+			render_square(cub3d, map_point, color);
+
 		}
-		printf("\n");
 	}
-			
 }
-
-static uint32_t	get_mini_map_color(t_cub3d *cub3d, t_vector *point)
-{
-	uint32_t color;
-
-	if (cub3d->map[])
-}
-
--- src/mandatory/render.c
+++ src/mandatory/render.c
@@ -1,9 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bedos-sa <bedos-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 17:48:13 by bedos-sa          #+#    #+#             */
+/*   Updated: 2024/03/09 19:04:34 by bedos-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 
 #include "cub3d.h"
 
-void	render_square(t_cub3d *cub3d, t_vector map_point)
+void	render_square(t_cub3d *cub3d, t_vector map_point, uint32_t color)
 {
-
+	for(size_t i = map_point.y; i < (map_point.y + MINI_MAP_TILE_SIZE); i++)
+		for(size_t j = map_point.x; j < (map_point.x + MINI_MAP_TILE_SIZE); j++)
+			mlx_put_pixel(((t_cub3d *)cub3d)->image, j, i, color);
+	
 }
